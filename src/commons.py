c56c064a872f200fdd8bcb79d936d2ee464f402c
Massimo Nocentini <massimo.nocentini@gmail.com>
2018-07-30 17:45:25 +0200

    Add comments to include code in my PhD thesis.

--- src/commons.py
+++ src/commons.py
@@ -10,29 +10,12 @@ def define(let, be, **kwds):
 
 @contextmanager
 def lift_to_Lambda(eq, return_eq=False, lhs_handler=lambda args: []):
-    '''
-    I lift an equational function def into a callable object.
 
-    >>> a = IndexedBase('a')
-    >>> aeq = Eq(a[n], n+a[n-1])
-    >>> with lift_to_Lambda(aeq, return_eq=True) as aEQ:
-    ...     arec = aEQ(n+1)
-    ... arec
-    Eq(a[n + 1], n + a[n] + 1)
-
-    >>> b = Function('b')
-    >>> beq = Eq(b(n), n+b(n-1))
-    >>> with lift_to_Lambda(beq, return_eq=True) as bEQ:
-    ...     brec = bEQ(n+1)
-    ... brec
-    Eq(b(n + 1), n + b(n) + 1))
-                
-    '''
     lhs = eq.lhs
-    args = (lhs.args[1:] if isinstance(lhs, Indexed) else 
-            lhs.args if isinstance(lhs, Function) else 
-            lhs_handler(lhs))
-    yield Lambda(args, eq if return_eq else eq.rhs)
+    args = (lhs.args[1:] if isinstance(lhs, Indexed) else           # get arguments with respect to the type of `lhs`
+            lhs.args     if isinstance(lhs, Function) else          # object; here we handle both function and
+            lhs_handler(lhs))                                       # subscript notations. Finally, `Lambda` is the
+    yield Lambda(args, eq if return_eq else eq.rhs)                 # class of callable objects in SymPy.
 
 def save_latex_repr(term, filename, iterable=False):
 
--- src/sequences.py
+++ src/sequences.py
@@ -25,84 +25,84 @@ def riordan_matrix_exponential(RA):
 
 def riordan_matrix_by_convolution(dim, d, h):
 
-    t = symbols('t')                                                # Local symbol to denote a fresh formal variable `t`.
+    t = symbols('t')                                                # Local symbol to denote a formal variable `t`.
 
     with lift_to_Lambda(d, return_eq=True) as D:                    # Lift equations `d` and `h` to become callables
-      with lift_to_Lambda(h, return_eq=True) as H:                  # objects, returning equations as well.
-        d_eq, h_eq = D(t), H(t)                                     # Evaluate both of them to get terms in the var `t`.
+      with lift_to_Lambda(h, return_eq=True) as H:                  # objects, returning equations as well, in order to
+        d_eq, h_eq = D(t), H(t)                                     # let both of them depend on variable `t`.
 
     @lru_cache(maxsize=None)
-    def column(j):                                                  # columns are memoized for the sake of efficiency
-        if not j: return d_eq
-        lhs = column(j-1).lhs * h_eq.lhs
-        rhs = column(j-1).rhs * h_eq.rhs
+    def column(j):                                                  # Columns are memoized for the sake of efficiency.
+        if not j: return d_eq                                       # Base case.
+        lhs = column(j-1).lhs * h_eq.lhs                            # Otherwise, use already computed column to build
+        rhs = column(j-1).rhs * h_eq.rhs                            # the current `lhs` and `rhs`.
         return Eq(lhs, rhs)
 
     @lru_cache(maxsize=None)
-    def C(j):
-        return column(j).rhs.series(t, n=dim).removeO()
+    def C(j):                                                       # Local function that performs Taylor expansion
+        return column(j).rhs.series(t, n=dim).removeO()             # of columns, which are symbolic terms up to now.
 
-    return lambda i, j: C(j).coeff(t, i).expand()          # return a lambda to be plugged in a `Matrix` ctor.
+    return lambda i, j: C(j).coeff(t, i).expand()                   # Return a lambda to be plugged in a `Matrix` ctor.
 
 def riordan_matrix_by_AZ_sequences(dim, seqs, init={(0,0):1},
                                    ctor=zeros, post=expand,
                                    lattice=None):
 
-    if not lattice: lattice = [(n, k) for n in range(1, dim)
-                                      for k in range(n+1)]
+    if not lattice: lattice = [(n, k) for n in range(1, dim)        # `lattice` plays the same role as in function
+                                      for k in range(n+1)]          # `riordan_matrix_by_recurrence`, see that.
 
-    Zseq, Aseq = seqs
-    t = symbols('t')
+    Zseq, Aseq = seqs                                               # Destruct `seqs` into objs denoting Z and A seqs.
+    t = symbols('t')                                                # Local formal variable `t`.
 
-    with lift_to_Lambda(Zseq) as Z, lift_to_Lambda(Aseq) as A:
-        Z_series = Z(t).series(t, n=dim).removeO()
-        A_series = A(t).series(t, n=dim).removeO()
+    with lift_to_Lambda(Zseq) as Z, lift_to_Lambda(Aseq) as A:      # Promote both sequences as callable objects to
+        Z_series = Z(t).series(t, n=dim).removeO()                  # evaluate both of them in `t` and then perform
+        A_series = A(t).series(t, n=dim).removeO()                  # two Taylor expansions with respect to `t`.
 
-    R = ctor(dim)
+    R = ctor(dim)                                                   # Init the matrix.
 
-    for cell, i in init.items(): R[cell] = i
+    for cell, i in init.items(): R[cell] = i                        # Set buondary values for combinations.
 
-    for n, k in lattice:
+    for n, k in lattice:                                            # Visit each cell according to the order `lattice`
 
-        if k:
-            v = sum(R[n-1, j] * A_series.coeff(t, n=i)
-                    for i, j in enumerate(range(k-1, dim)))
-        else:
-            v = sum(R[n-1, j] * Z_series.coeff(t, n=j)
-                    for j in range(dim))
+        if k:                                                       # If the cell lies not on the first column,
+            v = sum(R[n-1, j] * A_series.coeff(t, n=i)              # combine using `A_series` coefficients
+                    for i, j in enumerate(range(k-1, dim)))         # up to the cell in position `(k-1, dim-1)`.
+        else:                                                       # Otherwise, the cell lies on the first column,
+            v = sum(R[n-1, j] * Z_series.coeff(t, n=j)              # combine using `Z_series` coefficients
+                    for j in range(dim))                            # up to the cell in position `(k-1, dim-1)`.
 
-        R[n, k] = v
+        R[n, k] = v                                                 # Store the combination.
 
-    if callable(post): R = R.applyfunc(post)
+    if callable(post): R = R.applyfunc(post)                        # If some post processing is desired, do it.
 
-    return lambda n, k: R[n, k]
+    return lambda n, k: R[n, k]                                     # Return a lambda that uses the computed `R`.
 
 
 def riordan_matrix_by_recurrence(dim, rec, init={(0,0):1},
                                  ctor=zeros, post=expand,
                                  lattice=None):
 
-    if not lattice: lattice = [(n, k) for n in range(1, dim)
-                                      for k in range(n+1)]
+    if not lattice: lattice = [(n, k) for n in range(1, dim)        # `lattice` denotes the order in which coeffs in
+                                      for k in range(n+1)]          # the array will be computed; it can be plugged in.
 
-    R = ctor(dim)
+    R = ctor(dim)                                                   # Initial array as base for recursive construction.
 
-    for cell, i in init.items(): R[cell] = i
+    for cell, i in init.items(): R[cell] = i                        # Set boundary values for the recurrence `rec`.
 
-    for cell in lattice:
-        for comb_cell, v in rec(*cell).items():
+    for cell in lattice:                                            # Visit cells as they appear in the order `lattice`;
+        for comb_cell, v in rec(*cell).items():                     # then, get dependencies with respect the each cell.
 
             try:
-                comb = 1 if cell == comb_cell else R[comb_cell]
-                combined = v * comb
+                comb = 1 if cell == comb_cell else R[comb_cell]     # If it is possible to access the dependee cell,
+                combined = v * comb                                 # then perform the combination using the given `v`.
             except IndexError:
-                combined = 0
+                combined = 0                                        # Otherwise, take `0` because combination fails.
 
-            R[cell] += combined
+            R[cell] += combined                                     # Finally, accumulate the current combination.
 
-    if callable(post): R = R.applyfunc(post)
+    if callable(post): R = R.applyfunc(post)                        # If some post processing is desired, do it.
 
-    return lambda n, k: R[n, k]
+    return lambda n, k: R[n, k]                                     # Return a lambda that uses the computed `R`.
 
 def diagonal_matrix(seq, default=0):
 
@@ -180,15 +180,17 @@ def production_matrix(M, exp=False):
 
 def inspect(M):
 
-    P = production_matrix(M, exp=False)
-    C = production_matrix(M, exp=True)
+    P = production_matrix(M, exp=False)                             # Ordinary production matrix.
+    C = production_matrix(M, exp=True)                              # "Exponential" production matrix.
 
-    is_ord = all(P[:1-i, 1] == P[i-1:, i] for i in range(2, P.cols))
+    is_ord = all(P[:1-i, 1] == P[i-1:, i]                           # A RA is ordinary if columns of its PM
+                 for i in range(2, P.cols))                         # are shifted but equals.
 
-    diagonals = { d: [C[j+d,j] for j in range(1,C.rows-d)]
-                  for d in range(C.rows-3) }
+    diagonals = {d: [C[j+d,j] for j in range(1,C.rows-d)]           # Fetch matrix diagonals for the exponential case.
+                 for d in range(C.rows-3) }
 
-    is_exp = all(map(is_arithmetic_progression, diagonals.values()))
+    is_exp = all(map(is_arithmetic_progression,                     # A Riordan array is exponential if `diagonals`
+                     diagonals.values()))                           # are arithmetic progressions, all of them.
 
     return nature(is_ord, is_exp)
 
@@ -196,53 +198,54 @@ def inspect(M):
 def is_arithmetic_progression(prog):
 
     steps = len(prog)-1
-    for _ in range(steps):
-        prog = [(b-a).simplify() for a, b in zip(prog, prog[1:])]
+    for _ in range(steps):                                          # Reduce the list by consecutive differences,
+        prog = [(b-a).simplify() for a, b in zip(prog, prog[1:])]   # till a single object survives.
 
-    assert len(prog) == 1
+    assert len(prog) == 1                                           # Consistency check of the last comment.
 
-    return prog.pop() == 0
+    return prog.pop() == 0                                          # Finally, the reduced object has to vanish.
 
 
 def compositional_inverse(h_eq, y=symbols('y'), check=True):
 
-    spec, body = h_eq.lhs, h_eq.rhs
-    t, = spec.args
+    spec, body = h_eq.lhs, h_eq.rhs                                 # Destructuring function $h$ denoted by `h_eq`,
+    t, = spec.args                                                  # let `t` be the formal var of function $h$.
+                                                                    # $\bar{h}(h(t)) = t \leftrightarrow \left[ \bar{h}(y) = t\, | \, y = h(t) \right]$ allows us
+    sols = solve(Eq(y, body), t)                                    # to solve $y = h(t)$ with respect to $t$ because
+    for sol in sols:                                                # $h(t)$ is known, denoted by `body`. For each
+        L = Lambda(y, sol)                                          # solution `sol`, which depends on $y$, we build
+        if L(0) == 0:                                               # a callable object `L`. If it vanishes in $0$ and
+                                                                    # $\bar{h}(h(t))=t$, then it is a compositional
+            if check: assert L(body).simplify() == t                # inverse of $h$.
 
-    sols = solve(Eq(y, body), t)
-    for sol in sols:
-        L = Lambda(y, sol)
-        if L(0) == 0:
+            h_bar = Function(r'\bar{{ {} }}'.format(str(spec.func)))# Prepare the name for function $\bar{h}$ and
+            eq = Eq(h_bar(y), sol.factor())                         # build the corresponding equation that defines
+            return eq                                               # $\bar{h}(y)$, compositional inverse of $h(t)$.
 
-            if check: assert L(body).simplify() == t
-
-            h_bar = Function(r'\bar{{ {} }}'.format(str(spec.func)))
-            eq = Eq(h_bar(y), sol.factor())
-            return eq
-
-    raise ValueError
+    raise ValueError                                                # If the above code fails to return, raise an error.
 
 def group_inverse(d_eq, h_eq, post=identity, check=True):
 
-    t, y = symbols('t y')
+    t, y = symbols('t y')                                           # Formal symbols for symbolic functions $f$ and $g$.
     g_fn, f_fn = Function('g'), Function('f')
 
-    with lift_to_Lambda(d_eq, return_eq=False) as D:                    # Lift equations `d` and `h` to become callables
-      with lift_to_Lambda(h_eq, return_eq=True) as H:                  # objects, returning equations as well.
-        f_eq = compositional_inverse(H(t), y, check)
-        with lift_to_Lambda(f_eq, return_eq=False) as F:                  # objects, returning equations as well.
-            F_t = F(t)
-            g = post(1/D(F_t))
-            f = post(F_t)
+    with lift_to_Lambda(d_eq, return_eq=False) as D:                # Promote equations `d` and `h` to become callables
+      with lift_to_Lambda(h_eq, return_eq=True) as H:               # objects, returning equations as well.
+        f_eq = compositional_inverse(H(t), y, check)                # Let function $f$ be the compositional inverse of
+        with lift_to_Lambda(f_eq, return_eq=False) as F:            # function $h$, then promote it as callable and
+            F_t = F(t)                                              # (i) evaluate it at $t$;
+            g = post(1/D(F_t))                                      # (ii) build and refine $g$, the first function of
+            f = post(F_t)                                           # the new Ra; (iii) refine $f$, the second function.
 
-    g_eq, f_eq = Eq(g_fn(t), g.simplify()), Eq(f_fn(t), f.simplify())
+    g_eq = Eq(g_fn(t), g.simplify())                                # Build corresponding equation objects with
+    f_eq = Eq(f_fn(t), f.simplify())                                # simplified expressions.
 
-    if check:
-        with lift_to_Lambda(g_eq, return_eq=False) as G:                    # Lift equations `d` and `h` to become callables
-          with lift_to_Lambda(f_eq, return_eq=False) as F:                  # objects, returning equations as well.
-            H_rhs = H(t).rhs
-            assert (D(t)*G(H_rhs)).simplify() == 1
-            assert F(H_rhs).simplify() == t
+    if check:                                                       # If it is required to certify the computed result,
+        with lift_to_Lambda(g_eq, return_eq=False) as G:            # then promote the just built equations `g` and `f`
+          with lift_to_Lambda(f_eq, return_eq=False) as F:          # to perform group operation in order to check that
+            H_rhs = H(t).rhs                                        # it yields the identity element, which has
+            assert (D(t)*G(H_rhs)).simplify() == 1                  # (i) $1$ as first component and
+            assert F(H_rhs).simplify() == t                         # (ii) $t$ as second component.
 
     return g_eq, f_eq
 
