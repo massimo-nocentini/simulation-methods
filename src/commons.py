44ca452abaf1238d03a3b43a7202f2c6833ccb32
Massimo Nocentini <massimo.nocentini@gmail.com>
2018-10-28 16:31:08 +0100

    Adjust comments by removing empty spaces.
    [CI skip]

--- src/commons.py
+++ src/commons.py
@@ -4,19 +4,19 @@ from functools import partial
 
 def define(let, be, ctor=Eq, **kwds,):
 
-    if 'evaluate' not in kwds: kwds['evaluate'] = False # If `evaluate` is already given, use it as it is,
-    # otherwise set to `False` to preevent evaluation 
-                                                                    # by `Eq`, which implicitly do simplifications;
-    return ctor(let, be, **kwds)                                      # finally, return an equation object.
+    if 'evaluate' not in kwds:       # If `evaluate` is already given, use it # as it is,
+        kwds['evaluate'] = False     # otherwise set to `False` to preevent evaluation
+                                     # by `Eq`, which implicitly do simplifications;
+    return ctor(let, be, **kwds)     # finally, return an equation object.
 
 @contextmanager
 def lift_to_Lambda(eq, return_eq=False, lhs_handler=lambda args: []):
 
     lhs = eq.lhs
-    args = (lhs.args[1:] if isinstance(lhs, Indexed) else           # get arguments with respect to the type of `lhs`
-            lhs.args     if isinstance(lhs, Function) else          # object; here we handle both function and
-            lhs_handler(lhs))                                       # subscript notations. Finally, `Lambda` is the
-    yield Lambda(args, eq if return_eq else eq.rhs)                 # class of callable objects in SymPy.
+    args = (lhs.args[1:] if isinstance(lhs, Indexed) else    # get arguments wrt the type of `lhs` object;
+            lhs.args     if isinstance(lhs, Function) else   # here we handle both function and subscript
+            lhs_handler(lhs))                                # notations. Finally, `Lambda` is the
+    yield Lambda(args, eq if return_eq else eq.rhs)          # class of callable objects in SymPy.
 
 def save_latex_repr(term, filename, iterable=False):
 
@@ -33,7 +33,7 @@ def identity(*args):
     return args if len(args) > 1 else args[0]                       # Return the given arguments, simply.
 
 def foldl1(f, lst, key=identity):
-    
+
     first, *rest = lst                                              # Destructure `lst` into car and cdr.
     accumulated = key(first)                                        # Init the accumulator with the first element.
     while rest:                                                     # While there is element in the cdr,
@@ -49,8 +49,9 @@ class FEq(Eq):
         with lift_to_Lambda(self, **kwds) as feq:
             applied = feq(*args)
             if isinstance(applied, Eq):
-                subs = Subs(applied, self.lhs, self.rhs)  
-                setattr(subs, 'substitution', partial(lambda inner_self, *args, **kwds: self, subs))
+                subs = Subs(applied, self.lhs, self.rhs)
+                setattr(subs, 'substitution', partial(
+                    lambda inner_self, *args, **kwds: self, subs))
                 return subs
             else:
                 return applied
@@ -68,7 +69,7 @@ class FEq(Eq):
 
         def S(term):
             return term.subs(self, simultaneous=True)
-        
+
         if isinstance(arg, Eq):
             lhs = self.lhs if self.lhs == arg.lhs else S(arg.lhs)
             rhs = S(arg.rhs)
